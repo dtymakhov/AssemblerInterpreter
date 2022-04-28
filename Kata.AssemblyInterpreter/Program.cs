94afe7cb8b01639485d72b0e7f62229f264f210e
dtymakhov <dtymakhov@gmail.com>
2022-04-29 02:21:38 +0300

    Init project

---
+++ Kata.AssemblyInterpreter/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Kata.AssemblyInterpreter;
+
+public class AssemblerInterpreter
+{
+    private static Dictionary<string, int> _memory = new Dictionary<string, int>();
+
+    public static string Interpret(string input)
+    {
+        // Your code here!
+        return null;
+    }
+
+    public static void Main()
+    {
+    }
+}
