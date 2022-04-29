cae5c79e6f98452ae6fe305fc32a5f3aaa1410a4
dtymakhov <dtymakhov@gmail.com>
2022-04-30 00:37:26 +0300

    Functions and program end

--- Kata.AssemblyInterpreter/Program.cs
+++ Kata.AssemblyInterpreter/Program.cs
@@ -4,22 +4,24 @@ namespace Kata.AssemblyInterpreter;
 
 public class AssemblerInterpreter
 {
-    private static Dictionary<string, int> _memory = new Dictionary<string, int>();
-    private static Dictionary<string, string> _functions = new Dictionary<string, string>();
+    private static readonly Dictionary<string, int> Memory = new Dictionary<string, int>();
+    private static readonly Stack<int> Indexes = new Stack<int>();
     private static string _output = string.Empty;
     private static int _pointer = 0;
+    private static string[] _lines;
+    private const string End = "end";
 
-    public static string Interpret(string input)
+    public static string? Interpret(string input)
     {
         input = RemoveComments(input);
-        var lines = input.Split(new[] {Environment.NewLine}, StringSplitOptions.RemoveEmptyEntries);
 
-        for (; _pointer < lines.Length; _pointer++)
+        if (FindProgramEnd(input) == -1) return null;
+
+        _lines = input.Split(new[] {Environment.NewLine}, StringSplitOptions.RemoveEmptyEntries);
+
+        for (; _pointer < _lines.Length; _pointer++)
         {
-            var line = lines[_pointer];
-            var command = line.Split(' ');
-            var commandName = command[0];
-            var commandArgs = command.Skip(1).ToArray();
+            var (commandName, commandArgs) = ParseLine(_lines[_pointer]);
 
             switch (commandName)
             {
@@ -41,47 +43,69 @@ public class AssemblerInterpreter
                 case "sub":
                     Sub(commandArgs[0], commandArgs[1]);
                     break;
-                case "Mul":
+                case "mul":
                     Mul(commandArgs[0], commandArgs[1]);
                     break;
-                case "Div":
+                case "div":
                     Div(commandArgs[0], commandArgs[1]);
                     break;
-                case "Msg":
+                case "msg":
                     Msg(commandArgs[0], commandArgs.Skip(1).ToArray());
                     break;
-                case "Call":
+                case "call":
                     Call(commandArgs[0]);
                     break;
+                case "ret":
+                    Ret();
+                    break;
+                case "end":
+                    return _output;
             }
         }
 
         return _output;
     }
 
-    private static int GetValue(string s) => int.TryParse(s, out var tmp) ? tmp : _memory[s];
+    private static int GetValue(string s) => int.TryParse(s, out var tmp) ? tmp : Memory[s];
 
     #region Instructions
 
-    private static void Mov(string name, string value) => _memory[name] = GetValue(value);
+    private static void Mov(string name, string value) => Memory[name] = GetValue(value);
 
     private static void Jnz(string val1, string val2) => _pointer += GetValue(val1) != 0 ? GetValue(val2) - 1 : 0;
 
-    private static void Inc(string name) => _memory[name]++;
+    private static void Inc(string name) => Memory[name]++;
 
-    private static void Dec(string name) => _memory[name]--;
+    private static void Dec(string name) => Memory[name]--;
 
-    private static void Add(string name, string value) => _memory[name] += GetValue(value);
+    private static void Add(string name, string value) => Memory[name] += GetValue(value);
 
-    private static void Sub(string name, string value) => _memory[name] -= GetValue(value);
+    private static void Sub(string name, string value) => Memory[name] -= GetValue(value);
 
-    private static void Mul(string name, string value) => _memory[name] *= GetValue(value);
+    private static void Mul(string name, string value) => Memory[name] *= GetValue(value);
 
-    private static void Div(string name, string value) => _memory[name] /= GetValue(value);
+    private static void Div(string name, string value) => Memory[name] /= GetValue(value);
 
-    private static void Msg(string msg, params string[] parameters) => _output += string.Format(msg, parameters.Select(GetValue).ToArray());
+    private static void Msg(string msg, params string[] parameters)
+    {
+        var args = new List<string> {msg};
+        args.AddRange(parameters.Select(x => GetValue(x).ToString()).ToArray());
 
-    private static void Call(string label) => Interpret(_functions[label]);
+        _output += string.Join(' ', args);
+    }
+
+    private static void Call(string label)
+    {
+        Indexes.Push(_pointer);
+        var line = _lines.FirstOrDefault(l => l.StartsWith(label));
+        var funcIndex = Array.IndexOf(_lines, line);
+        _pointer = funcIndex;
+    }
+
+    private static void Ret()
+    {
+        _pointer = Indexes.Pop();
+    }
 
     private static string RemoveComments(string input)
     {
@@ -90,11 +114,43 @@ public class AssemblerInterpreter
         return output;
     }
 
+    private static int FindProgramEnd(string input)
+    {
+        return input.IndexOf(End, StringComparison.Ordinal);
+    }
+
+    private static (string, string[]) ParseLine(string line)
+    {
+        var trimmedLine = line.Trim();
+        var index = trimmedLine.IndexOf(" ", StringComparison.Ordinal);
+
+        if (index == -1) return (trimmedLine, Array.Empty<string>());
+
+        var commandName = trimmedLine[..index];
+        var commandArgs = trimmedLine[index..].Split(',');
+
+        for (var i = 0; i < commandArgs.Length; i++)
+        {
+            if (commandArgs[i].Contains('\'', StringComparison.Ordinal))
+            {
+                commandArgs[i] = commandArgs[i].Trim('\'', ' ');
+            }
+            else
+            {
+                commandArgs[i] = commandArgs[i].Trim();
+            }
+        }
+
+        return (commandName, commandArgs);
+    }
+
     #endregion
 
     public static void Main()
     {
-        Interpret(
-            "\nmov   a, 81         ; value1\nmov   b, 153        ; value2\ncall  init\ncall  proc_gcd\ncall  print\nend\n\nproc_gcd:\n    cmp   c, d\n    jne   loop\n    ret\n\nloop:\n    cmp   c, d\n    jg    a_bigger\n    jmp   b_bigger\n\na_bigger:\n    sub   c, d\n    jmp   proc_gcd\n\nb_bigger:\n    sub   d, c\n    jmp   proc_gcd\n\ninit:\n    cmp   a, 0\n    jl    a_abs\n    cmp   b, 0\n    jl    b_abs\n    mov   c, a            ; temp1\n    mov   d, b            ; temp2\n    ret\n\na_abs:\n    mul   a, -1\n    jmp   init\n\nb_abs:\n    mul   b, -1\n    jmp   init\n\nprint:\n    msg   'gcd(', a, ', ', b, ') = ', c\n    ret\n");
+        var result = Interpret(
+            "\n; My first program\nmov  a, 5\ninc  a\ncall function\nmsg  '(5+1)/2 = ', a    ; output message\nend\n\nfunction:\n    div  a, 2\n    ret\n");
+
+        Console.WriteLine(result);
     }
 }
